2fb435201b81340446030294369648106cbbcff4
Sahibi Miranshah <samirans@microsoft.com>
2020-06-23 22:46:24 -0700

    Add project files.

---
+++ MyUWPApp/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace MyUWPApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Random rand;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            DebugPrint();
+            rand = new Random();
+        }
+
+        public void DebugPrint()
+        {
+            System.Diagnostics.Debug.WriteLine("Debug Print --- Hello!");
+        }
+
+        private byte[] GetRandomBytes(int n)
+        {
+            //  Fill an array of bytes of length "n" with random numbers.
+            var randomBytes = new byte[n];
+            rand.NextBytes(randomBytes);
+            return randomBytes;
+        }
+
+        private void SayHelloButton_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] rgb = GetRandomBytes(3);
+
+            //  Create a solid color brush using the three random numbers.
+            var randomColorBrush = new SolidColorBrush(Color.FromArgb(255, rgb[0], rgb[1], rgb[2]));
+
+            //  Set both the text color and the text box border to the random color.
+            textBox1.BorderBrush = randomColorBrush;
+            textBox1.Foreground = randomColorBrush;
+        }
+    }
+}
